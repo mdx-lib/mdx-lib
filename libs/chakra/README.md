b770d6e0e4f6cd726b195cab638f188ddb330652
toaru <83337042+toaru@users.noreply.github.com>
2021-12-20 02:15:19 +0900

    feat: chakra lib
    npx nx g @nrwl/react:lib chakra --publishable --importPath=@mdx-lib/chakra

---
+++ libs/chakra/README.md
@@ -0,0 +1,7 @@
+# chakra
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test chakra` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/chakra/src/lib/chakra.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ChakraProps {}
+
+export function Chakra(props: ChakraProps) {
+  return (
+    <div>
+      <h1>Welcome to Chakra!</h1>
+    </div>
+  );
+}
+
+export default Chakra;
