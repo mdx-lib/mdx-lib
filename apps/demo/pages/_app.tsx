618cda73cbc38e2b822c9a29e7211adaa2d14016
toaru <83337042+toaru@users.noreply.github.com>
2021-12-20 02:04:10 +0900

    chore: remove css

--- apps/demo/pages/_app.tsx
+++ apps/demo/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
 import { ChakraProvider } from '@chakra-ui/react';
 
 function CustomApp({ Component, pageProps }: AppProps) {
@@ -10,9 +9,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Welcome to demo!</title>
       </Head>
       <ChakraProvider>
-        <main className="app">
-          <Component {...pageProps} />
-        </main>
+        <Component {...pageProps} />
       </ChakraProvider>
     </>
   );
