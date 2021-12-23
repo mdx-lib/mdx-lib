5debd7c2d07527820f10c50eda29f3f8408a51dc
Toaru <83337042+toaru@users.noreply.github.com>
2021-12-23 09:13:34 +0900

    feat: setup mdx (#15)
    * deps: yarn add @next/mdx @mdx-js/loader
    
    * chore: update next.config.js with mdx
    
    * chore: add sample mdx

--- apps/demo/next.config.js
+++ apps/demo/next.config.js
@@ -1,10 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
@@ -12,4 +20,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withNx(nextConfig);
+module.exports = withNx(withMDX(nextConfig));
