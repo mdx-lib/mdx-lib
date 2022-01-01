07b7fe728ca39ce349b756b710d0c8cb61231976
toaru <83337042+toaru@users.noreply.github.com>
2022-01-01 19:31:44 +0900

    feat: heading and paragraph

--- libs/chakra/src/lib/ChakraMDXProvider.tsx
+++ libs/chakra/src/lib/ChakraMDXProvider.tsx
@@ -8,11 +8,12 @@ import { chakra } from '@chakra-ui/react';
 
 const components = {
   // img: ResponsiveImage,
-  h1: (props: unknown) => (
-    <chakra.h1 apply="mdx.h1" {...props} fontSize="5xl" />
-  ),
-  // h2: Heading.H2,
-  // p: Text,
+  h1: (props: unknown) => <chakra.h1 {...props} fontSize="5xl" />,
+  h2: (props: unknown) => <chakra.h2 {...props} fontSize="4xl" />,
+  h3: (props: unknown) => <chakra.h3 {...props} fontSize="3xl" />,
+  h4: (props: unknown) => <chakra.h4 {...props} fontSize="2xl" />,
+  h5: (props: unknown) => <chakra.h5 {...props} fontSize="xl" />,
+  p: (props: unknown) => <chakra.p {...props} fontSize="md" />,
   // code: Pre,
   // inlineCode: Code,
 };
