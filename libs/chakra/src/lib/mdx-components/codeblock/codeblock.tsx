21c0c595c10a47ecb1e99be18829e5c93cf3a070
toaru <83337042+toaru@users.noreply.github.com>
2022-01-09 18:38:24 +0900

    refactor code block (remove live editor)

--- libs/chakra/src/lib/mdx-components/codeblock/codeblock.tsx
+++ libs/chakra/src/lib/mdx-components/codeblock/codeblock.tsx
@@ -1,64 +1,19 @@
-import { Box, useBoolean } from '@chakra-ui/react';
-import dynamic from 'next/dynamic';
-import theme from 'prism-react-renderer/themes/nightOwl';
-import React, { useEffect } from 'react';
-import CodeContainer from './code-container';
-import CopyButton from './copy-button';
-import Highlight from './highlight';
-
-const ReactLiveBlock = dynamic(() => import('./react-live-block'));
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+import theme from 'prism-react-renderer/themes/nightOwl'
+import CodeContainer from './code-container'
+import CopyButton from './copy-button'
+import Highlight from './highlight'
 
 function CodeBlock(props) {
-  const [isMounted, { on }] = useBoolean();
-  useEffect(
-    /**
-     * Lazily-load <ReactLiveBlock /> to save bundle size.
-     */
-    on,
-    [on]
-  );
-  const {
-    className,
-    live = true,
-    manual,
-    render,
-    children,
-    viewlines,
-    ln,
-    mountStylesheet = false,
-  } = props.children.props;
+  const { className, children, viewlines, ln } = props.children.props
 
-  const _live = live === 'true' || live === true;
-
-  const language = className?.replace(/language-/, '');
-  const rawCode = children.trim();
-
-  const reactLiveBlockProps = {
-    rawCode,
-    language,
-    theme,
-    noInline: manual,
-    mountStylesheet,
-  };
-
-  if (isMounted && language === 'jsx' && _live === true) {
-    return <ReactLiveBlock editable {...reactLiveBlockProps} />;
-  }
-
-  if (isMounted && render) {
-    /**
-     * @TODO Not sure if this is even used?
-     */
-    return (
-      <div style={{ marginTop: 32 }}>
-        <ReactLiveBlock editable={false} {...reactLiveBlockProps} />
-      </div>
-    );
-  }
+  const language = className?.replace(/language-/, '')
+  const rawCode = children.trim()
 
   return (
-    <Box position="relative" zIndex="0">
-      <CodeContainer px="0" overflow="hidden">
+    <Box position='relative' zIndex='0'>
+      <CodeContainer px='0' overflow='hidden'>
         <Highlight
           codeString={rawCode}
           language={language}
@@ -67,9 +22,9 @@ function CodeBlock(props) {
           showLines={viewlines}
         />
       </CodeContainer>
-      <CopyButton top="4" code={rawCode} />
+      <CopyButton top='4' code={rawCode} />
     </Box>
-  );
+  )
 }
 
-export default CodeBlock;
+export default CodeBlock
