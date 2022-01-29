f4e54c33b21d6e0124e83a1c890b7dc35e6aad75
Toaru <83337042+toaru@users.noreply.github.com>
2022-01-29 21:11:56 +0900

    chore: replace sample index (#82)
    * chore: use index.mdx
    
    * test: replace unit test to e2e rendering test
    
    * chore: vt command
    
    * chore: remove sample button
    
    * chore: fix lint error

--- apps/demo-e2e/src/integration/app.spec.ts
+++ apps/demo-e2e/src/integration/app.spec.ts
@@ -1,12 +1,10 @@
 describe('demo', () => {
-  beforeEach(() => cy.visit('/sample'))
-
   it('visual regression test', () => {
-    cy.visit('/sample').screenshot('sample')
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.visit('/').wait(2000).screenshot('index')
 
     // // Custom command example, see `../support/commands.ts` file
     // cy.login('my-email@something.com', 'myPassword');
-    //
     // // Function helper example, see `../support/app.po.ts` file
     // getGreeting().contains('Welcome demo');
   })
