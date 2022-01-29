d2c34519447c1d262605772544873aff209a87cd
toaru <83337042+toaru@users.noreply.github.com>
2022-01-29 20:51:43 +0900

    test: replace unit test to e2e rendering test

--- apps/demo-e2e/src/integration/app.spec.ts
+++ apps/demo-e2e/src/integration/app.spec.ts
@@ -1,12 +1,9 @@
 describe('demo', () => {
-  beforeEach(() => cy.visit('/sample'))
-
   it('visual regression test', () => {
-    cy.visit('/sample').screenshot('sample')
+    cy.visit('/').wait(2000).screenshot('index')
 
     // // Custom command example, see `../support/commands.ts` file
     // cy.login('my-email@something.com', 'myPassword');
-    //
     // // Function helper example, see `../support/app.po.ts` file
     // getGreeting().contains('Welcome demo');
   })
