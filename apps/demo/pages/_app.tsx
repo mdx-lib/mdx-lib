64f620a908a72f3bfae39c7f500129c8d23848c0
Toaru <83337042+toaru@users.noreply.github.com>
2022-01-30 23:14:05 +0900

    feat: format command (#89)
    * feat: add format command
    
    * feat: add prettier ignore
    
    * chore: run "yarn format"

--- apps/demo/pages/_app.tsx
+++ apps/demo/pages/_app.tsx
@@ -1,8 +1,8 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { ChakraProvider } from '@chakra-ui/react';
-import { ChakraMDXProvider } from '@mdx-lib/chakra';
-import theme from '../theme';
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraMDXProvider } from '@mdx-lib/chakra'
+import theme from '../theme'
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -16,7 +16,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
         </ChakraMDXProvider>
       </ChakraProvider>
     </>
-  );
+  )
 }
 
-export default CustomApp;
+export default CustomApp
