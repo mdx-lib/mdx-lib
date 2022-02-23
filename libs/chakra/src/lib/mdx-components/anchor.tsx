cdab7340e0a572a3a486499b691ff004a57f7035
Toaru <83337042+toaru@users.noreply.github.com>
2022-02-23 10:50:24 +0900

    chore: remove unused import (#86)

--- libs/chakra/src/lib/mdx-components/anchor.tsx
+++ libs/chakra/src/lib/mdx-components/anchor.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { chakra } from '@chakra-ui/react'
-import { LegacyRef, Ref } from 'react'
+import { Ref } from 'react'
 
 export const Anchor = React.forwardRef(
   (props: unknown, ref: Ref<HTMLAnchorElement>) => (
